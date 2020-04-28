c07e43fbbb79f4c6e33769744523af87f94d25d4
Tom Mulkins <tom.mulkins@gmail.com>
2020-04-27 20:44:15 -0400

    fix linting

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ const App = () => {
             <input
               id="search"
               type="text"
-              placeHolder="Who are you looking for?"
+              placeholder="Who are you looking for?"
               className="search-bar"
             />
           </label>
           <label htmlFor="sort">
             <h2>Sort by</h2>
-            <select id="sort" value={} className="sort-by">
+            <select id="sort" value="" className="sort-by">
               <option>Firstname</option>
               <option>Lastname</option>
             </select>
@@ -43,7 +43,7 @@ const App = () => {
               src="https://media-exp1.licdn.com/dms/image/C5603AQEsP0RffvxWKA/profile-displayphoto-shrink_200_200/0?e=1593648000&v=beta&t=KUpFO5kY1Jb8CwxR_qAFJ9xOod08xsGFgxs8Z2vXybw"
               height="130"
               width="130"
-              alt="Profile Image"
+              alt="Profile"
               className="card-image"
             />
 
@@ -57,8 +57,15 @@ const App = () => {
       </div>
 
       <footer className="footer">
-        👷‍♂️ Made by{" "}
-        <a href="https://www.linkedin.com/in/tom-mulkins/" target="_blank">
+        <span role="img" aria-label="worker">
+          👷‍♂️
+        </span>
+        Made by{" "}
+        <a
+          href="https://www.linkedin.com/in/tom-mulkins/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Tom Mulkins
         </a>
       </footer>
