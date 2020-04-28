fda93dc79ce746198991a55897f568475ff3d62e
Tom Mulkins <tom.mulkins@gmail.com>
2020-04-28 16:46:08 -0400

    todo: sync state for contact change

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserEdit } from "@fortawesome/free-solid-svg-icons";
 import Modal from "./Modal";
 
 const Card = ({ position, contact }) => {
   const [showModal, setShowModal] = useState(false);
-
-  const firstName = contact.name.first;
-  const lastName = contact.name.last;
-  const picture = contact.picture.large || "https://via.placeholder.com/150";
-  const email = contact.email;
-  const phone = contact.phone;
-  const location = `${contact.location.city}, ${contact.location.state}`;
+  const [newContact, setNewContact] = useState({ ...contact });
 
   const toggleModal = () => {
     setShowModal(!showModal);
   };
+
+  useEffect(() => {
+    setNewContact(() => {
+      return { ...contact };
+    });
+  }, [contact]);
 
   return (
     <div className="card">
@@ -26,11 +26,11 @@ const Card = ({ position, contact }) => {
           className="card-icon"
           onClick={toggleModal}
         />
-        <div className="card-name">{`${firstName} ${lastName}`}</div>
+        <div className="card-name">{`${contact.name.first} ${contact.name.last}`}</div>
       </div>
 
       <img
-        src={picture}
+        src={contact.picture.large}
         height="130"
         width="130"
         alt="Profile"
@@ -39,11 +39,11 @@ const Card = ({ position, contact }) => {
 
       <div className="card-body">
         <div className="card-details">
-          {email}
+          {contact.email}
           <br />
-          {phone}
+          {contact.phone}
           <br />
-          {location}
+          {`${contact.location.city}, ${contact.location.state}`}
           <br />
         </div>
       </div>
@@ -51,10 +51,78 @@ const Card = ({ position, contact }) => {
         <Modal>
           <div>
             <h1>
-              Editing #{position} - {firstName} {lastName}
+              Editing #{position} - {contact.name.first} {contact.name.last}
             </h1>
+            <form action="" onSubmit={(e) => e.preventDefault()}>
+              First Name
+              <br />
+              <input
+                type="text"
+                value={newContact.name.first}
+                onChange={(e) =>
+                  setNewContact({ name: { first: e.target.value.trim() } })
+                }
+              />
+              <br />
+              Last Name
+              <br />
+              <input
+                type="text"
+                value={newContact.name.last}
+                onChange={(e) =>
+                  setNewContact({ name: { last: e.target.value.trim() } })
+                }
+              />
+              <br />
+              Email
+              <br />
+              <input
+                type="text"
+                value={newContact.email}
+                onChange={(e) =>
+                  setNewContact({ email: e.target.value.trim() })
+                }
+              />
+              <br />
+              Phone
+              <br />
+              <input
+                type="text"
+                value={newContact.phone}
+                onChange={(e) =>
+                  setNewContact({ phone: e.target.value.trim() })
+                }
+              />
+              <br />
+              City
+              <br />
+              <input
+                type="text"
+                value={newContact.location.city}
+                onChange={(e) =>
+                  setNewContact({ location: { city: e.target.value.trim() } })
+                }
+              />
+              <br />
+              State
+              <br />
+              <input
+                type="text"
+                value={newContact.location.state}
+                onChange={(e) =>
+                  setNewContact({ location: { state: e.target.value.trim() } })
+                }
+              />
+            </form>
             <div className="buttons">
-              <button onClick={toggleModal}>Cancel</button>
+              <button type="submit">Save</button>
+              <button
+                type="button"
+                style={{ backgroundColor: "#ccc", color: "#000" }}
+                onClick={toggleModal}
+              >
+                Cancel
+              </button>
             </div>
           </div>
         </Modal>
